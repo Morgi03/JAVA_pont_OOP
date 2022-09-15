160b144b6265dabb114676e8f809a0d8a1d192e3
Morgi03 <112927920+Morgi03@users.noreply.github.com>
2022-09-15 09:08:44 +0200

    Initial commit

---
+++ pont/Pont.java
@@ -0,0 +1,34 @@
+package hu.petrik.firstjavaoop.pont;
+
+public class Pont {
+    private int x;
+    private int y;
+
+    public Pont() {
+        x = 0;
+        y = 0;
+    }
+
+    public Pont(int x, int y) {
+        x = this.x;
+        y = this.y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Pont(int n) {
+        this.x = (int) (Math.random() * (2 * n) + 1 ) - n;
+    }
+
+    @Override
+    public String toString() {
+        // return "Pont{" + "x=" + x + ", y=" + y + '}';
+        return String.format("(°d,°d)", this.x, this.y);
+    }
+}
